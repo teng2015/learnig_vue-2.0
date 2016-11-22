a2201a87c01a55ecaa22505becf54a548c121b07
xjq熊剑桥 <jqxiong@Ctrip.com>
2016-11-22 21:55:54 +0800

    update chunkFileName

--- build/webpack.base.conf.js
+++ build/webpack.base.conf.js
@@ -19,7 +19,8 @@ module.exports = {
   output: {
     path: config.build.assetsRoot,
     publicPath: process.env.NODE_ENV === 'production' ? config.build.assetsPublicPath : config.dev.assetsPublicPath,
-    filename: '[name].js'
+    filename: '[name].js',
+    chunkFilename: '[id].[hash].js'
   },
   resolve: {
     extensions: ['', '.js', '.vue'],
